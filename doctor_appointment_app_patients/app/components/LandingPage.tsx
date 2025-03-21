0787a823c915f0d76ee8d0a78c01ff8365a02861
TTNamar2003 <amar.singh@tothenew.com>
2025-03-21 14:03:24 +0530

    updated code

--- doctor_appointment_app_patients/app/components/LandingPage.tsx
+++ doctor_appointment_app_patients/app/components/LandingPage.tsx
@@ -3,12 +3,16 @@ import styles from '@/app/styles/LandingPage.module.css'
 import Image from 'next/image'
 import landingPageImg from '@/public/images/landingPageImg.svg'
 import LandingPageContent from './LandingPageContent'
+import ScheduleAppointment from './ScheduleAppointment'
 export default function LandingPage() {
     return (
         <div className={styles.landingPage_container}>
             <LandingPageContent/>
             <section className={styles.landingPage_img_section}>
-            <Image src={landingPageImg}  alt="landing page img"  />
+            {/* <Image src={landingPageImg}  alt="landing page img"  /> */}
+            <div className={styles.appointment_component}>
+            <ScheduleAppointment/>
+            </div>
             </section>
         </div>
     )

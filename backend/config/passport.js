3582d3d600dfe384489b2daee0b7e911316eaa13
TTNamar2003 <amar.singh@tothenew.com>
2025-03-31 20:48:08 +0530

    updated backend

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -53,4 +53,11 @@ passport.use(
   )
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 export default passport;
--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false },
+    cookie: { secure: true },
   })
 );
 
